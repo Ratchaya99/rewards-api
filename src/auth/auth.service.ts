8f41f758cf1f226a988fe1853a878f94c0ba930d
Ratchaya <ratchaya.gun@gmail.com>
2026-07-20 18:13:39 +0700

    feat: implement user registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
+import { RegisterResponseDto } from './dto/register-response.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    const existingUser = await this.prismaService.user.findUnique({
+      where: { email: registerDto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+    const userRole = await this.prismaService.role.findUnique({
+      where: {
+        name: 'USER',
+      },
+    });
+
+    if (!userRole) {
+      throw new Error('USER role not found');
+    }
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email: registerDto.email,
+        password: hashedPassword,
+        firstName: registerDto.firstName,
+        lastName: registerDto.lastName,
+        roleId: userRole.id,
+      },
+    });
+
+    const response: RegisterResponseDto = {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
+
+    return response;
+  }
 }
---
+++ src/auth/dto/register-response.dto.ts
@@ -0,0 +1,6 @@
+export class RegisterResponseDto {
+  id!: string;
+  email!: string;
+  firstName!: string;
+  lastName!: string;
+}
